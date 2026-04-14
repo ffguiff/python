3e2f264bd39f00434c882f0d3dfab6137c5dac71
Guilherme Ferreira <90099945+ffguiff@users.noreply.github.com>
2026-04-14 20:30:39 -0300

    Update manipulacaoDeArq.py

--- manipulacaoDeArq.py
+++ manipulacaoDeArq.py
@@ -2,7 +2,7 @@ conteudo = open("teste.txt","a") #a = inserir alguma coisa na utima linha
 conteudo.write("uma linha qualquer\n") # escrever na tela
 conteudo.write("segunda linha\n")
 conteudo.close()
-
+  
 '''ler'''
 # conteudo = open("teste.txt", "r")
 # print(conteudo.read())
